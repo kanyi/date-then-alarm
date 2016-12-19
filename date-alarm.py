dcb72381d98b295452f1a16ce894d2aa139c880d
Balázs Attila <kanyi@users.noreply.github.com>
2016-12-19 15:16:31 +0100

    Update date-alarm.py
    Write to file added

--- date-alarm.py
+++ date-alarm.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 """
-Small program for ask a date and then ask days number and combine these two data.
-Later create an alarm for this.
+Small program for ask a date and then ask days number and combine these two data. 
+Write out to the disk, and show some information.
+Later create an alarm for this.....
 """
 #for string conversion (str to int)
 #https://www.cyberciti.biz/faq/python-convert-string-to-int-functions/
@@ -10,11 +11,18 @@ Later create an alarm for this.
 #https://pymotw.com/3/datetime/
 #https://stackoverflow.com/questions/12772057/how-to-format-date-in-iso-using-python
 #https://automatetheboringstuff.com/chapter15/
+#input-output
+#http://www.python-course.eu/python3_file_management.php
+#http://www.diveintopython3.net/files.html
+#https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
+#http://www.theunixschool.com/2013/08/python-how-to-write-list-to-file.html
+#http://www.python-course.eu/python3_print.php
 
 import datetime
 import calendar
 now=datetime.datetime.now() #precise timestamp
 ma = datetime.date.today() #only the date
+list_of_hebrakolas=[]
 
 print("a mai dátum", ma, "pontosabban:", now)
 hebrakolas_datuma=input("hebrákolás dátuma (YYYY-mm-dd):")
@@ -27,5 +35,18 @@ one_day = datetime.timedelta(days=1) #just an example
 alarm_days = datetime.timedelta(days = alarm_days)
 date_of_alarm = hebrakolas_datuma + alarm_days
 
+list_of_hebrakolas.extend([hebrakolas_datuma, alarm_days, date_of_alarm])
+hebrakolasok=open("hebrakolasok", 'a')
+print (list_of_hebrakolas, end="\n", file=hebrakolasok)
+hebrakolasok.close()
+
+#the write method - shit
+#hebrakolasok = open("hebrakolasok.txt", 'w') #ha 'w' így mindig felülírja a dolgokat 'a' jobb
+#for hbr in list_of_hebrakolas:
+#    hebrakolasok.write(hbr+';')
+#hebrakolasok.write('\n')
+#hebrakolasok.close()
+
 print (calendar.TextCalendar(calendar.MONDAY).formatmonth(date_of_alarm.year, date_of_alarm.month, 1, 1))
 print ('alarm day:', date_of_alarm.strftime('%Y-%m-%d'))
+print ("list_of_hebrakolas:", list_of_hebrakolas)
